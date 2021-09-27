afab36c7735edd2026fad100d61017abdb854678
cyanchill <anthonyliang9@gmail.com>
2021-09-27 15:08:33 -0400

    outline of the possible components to be used

---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+
+import { CVContext } from "../CVContext";
+
+const Form = () => {
+  const { cvInfo, setCVInfo } = useContext(CVContext);
+
+  /* 
+    Basically a lot of fields
+    - If use use the components, we can simplify things
+  */
+  return <div>
+
+
+  </div>;
+};
+
+export default Form;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import Hero from "./Hero";
+import Skills from "./Skills";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <Hero />
+      <Skills />
+    </div>
+  );
+};
+
+export default Sidebar;
