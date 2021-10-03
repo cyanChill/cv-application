cba989692a0928db236ca29cd4308fe524548b15
cyanchill <anthonyliang9@gmail.com>
2021-10-02 22:49:36 -0400

    add conditional loading for what gets displayed (form or cv)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useContext } from "react";
+
+import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
+import Form from "./components/form/Form";
+
+import { CVContext } from "./CVContext";
+import "./styles/App.style.css";
 
 function App() {
+  const { preview, setPreview } = useContext(CVContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!preview ? (
+        <Form />
+      ) : (
+        <>
+          <Sidebar /> <MainContent />
+        </>
+      )}
     </div>
   );
 }
