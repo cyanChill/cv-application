a31deea2b913896d96c727fed3a2265aaf95aa40
cyanchill <anthonyliang9@gmail.com>
2021-10-02 22:26:23 -0400

    move contents to form component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,49 +1,10 @@
 import { useContext } from "react";
 import { CVContext } from "../CVContext";
-import uniqid from "uniqid";
-
-import "../styles/Experience.style.css";
-import Job from "./Job";
 
 const Experience = () => {
-  const { preview, cvInfo, setCVInfo } = useContext(CVContext);
+  const { cvInfo, setCVInfo } = useContext(CVContext);
 
-  const addJob = () => {
-    const newJob = {
-      title: "",
-      company: "",
-      description: "",
-      startDate: "",
-      endDate: "",
-      id: uniqid(),
-    };
-
-    setCVInfo({
-      ...cvInfo,
-      experience: [...cvInfo.experience, newJob],
-    });
-  };
-
-  const deleteJob = (id) => {
-    const newJobs = cvInfo.experience.filter((job) => job.id !== id);
-
-    setCVInfo({
-      ...cvInfo,
-      experience: newJobs,
-    });
-  };
-
-  return (
-    <div>
-      <h2>Experience:</h2>
-      <div className="experience-list">
-        {cvInfo.experience.map((job) => (
-          <Job key={job.id} experience={job} deleteJob={deleteJob} />
-        ))}
-        <button onClick={addJob}>Add More</button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Experience;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -16,36 +16,7 @@ const Skills = () => {
     - Maybe how we do this is that when we click "add skill", it adds an item to the cvInfo.skills array which will then render onto the page
   */
 
-  const addSkill = () => {
-    const newSkill = {
-      name: "",
-      rating: 5,
-      id: uniqid(),
-    };
-
-    setCVInfo({
-      ...cvInfo,
-      skills: [...cvInfo.skills, newSkill],
-    });
-  };
-
-  const deleteSkill = (id) => {
-    const newSkills = cvInfo.skills.filter((skill) => skill.id !== id);
-
-    setCVInfo({
-      ...cvInfo,
-      skills: newSkills,
-    });
-  };
-
-  return (
-    <div className="skills-list">
-      {cvInfo.skills.map((skill) => (
-        <Skill key={skill.id} skill={skill} deleteSkill={deleteSkill} />
-      ))}
-      <button onClick={addSkill}>Add More</button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Skills;
