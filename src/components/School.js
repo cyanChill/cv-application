00c5b29c8955dd052c0cc217f8eaf075188b2725
cyanchill <anthonyliang9@gmail.com>
2021-10-03 21:54:53 -0400

    general component styling completed

--- src/components/School.js
+++ src/components/School.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { CVContext } from "../CVContext";
 
-import { FaTrash } from "react-icons/fa";
+import { getTermDate } from "../helper/utilitiy";
 
 const School = ({ deleteSchool, education }) => {
   const { preview, cvInfo, setCVInfo } = useContext(CVContext);
@@ -22,31 +22,45 @@ const School = ({ deleteSchool, education }) => {
     });
   };
 
+  if (!preview) {
+    return (
+      <div className="school">
+        <div className="text flex-col">
+          <input
+            type="text"
+            placeholder="School Name"
+            value={school}
+            onChange={(e) => updateField(e, "school")}
+          />
+          <input
+            type="text"
+            placeholder="Degree Name"
+            value={degree}
+            onChange={(e) => updateField(e, "degree")}
+          />
+        </div>
+        <div className="dates flex-col">
+          <span className="field-title">Start Date:</span>
+          <input type="date" value={startDate} onChange={(e) => updateField(e, "startDate")} />
+          <span className="field-title">End Date:</span>
+          <input type="date" value={endDate} onChange={(e) => updateField(e, "endDate")} />
+          <button className="btn trash" onClick={() => deleteSchool(id)}>
+            Delete
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="school">
-      <div className="text flex-col">
-        <input
-          type="text"
-          placeholder="School Name"
-          value={school}
-          onChange={(e) => updateField(e, "school")}
-        />
-        <input
-          type="text"
-          placeholder="Degree Name"
-          value={degree}
-          onChange={(e) => updateField(e, "degree")}
-        />
-      </div>
-      <div className="dates flex-col">
-        <span className="field-title">Start Date:</span>
-        <input type="date" value={startDate} onChange={(e) => updateField(e, "startDate")} />
-        <span className="field-title">End Date:</span>
-        <input type="date" value={endDate} onChange={(e) => updateField(e, "endDate")} />
-        <button className="btn trash" onClick={() => deleteSchool(id)}>
-          Delete
-        </button>
-      </div>
+      <h1 className="school-name">
+        {school}
+        {degree ? ` - ${degree}` : ""}
+      </h1>
+      <p className="duration">
+        {getTermDate(startDate)} - {getTermDate(endDate)}
+      </p>
     </div>
   );
 };
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,70 @@
-import Hero from "./Hero";
-import Skills from "./Skills";
+import { useContext, useRef, useEffect } from "react";
+import { CVContext } from "../CVContext";
+import Inputs from "../inputType";
+
+import "../styles/Sidebar.style.css";
 
 const Sidebar = () => {
+  const { cvInfo } = useContext(CVContext);
+  const { name, title, image, contact, skills } = cvInfo;
+  const profileImg = useRef(null);
+
+  const isContactEmpty = Object.values(contact).every((val) => val === "");
+  const isSkillEmpty = skills.every((val) => val.name === "");
+
+  const showProfileImg = () => {
+    if (image) {
+      profileImg.current.src = URL.createObjectURL(image);
+      profileImg.current.onload = () => URL.revokeObjectURL(profileImg.current.src);
+    }
+  };
+
+  useEffect(() => {
+    showProfileImg();
+  }, []);
+
   return (
-    <div className="sidebar">
-      <Hero />
-      <Skills />
+    <div id="sidebar">
+      {image ? <img alt="Profile" ref={profileImg} id="profile" /> : ""}
+      <p id="name">{name}</p>
+      <p id="title">{title}</p>
+      {isContactEmpty ? (
+        ""
+      ) : (
+        <div id="contacts">
+          <h2 className="section-title">Contact</h2>
+          {Inputs.map((type, idx) => (
+            <p key={idx} className="contact-field">
+              {type.type === "github"
+                ? `github.com/${contact[type.type]}`
+                : type.type === "linkedin"
+                ? `linkedin.com/in/${contact[type.type]}`
+                : contact[type.type]}
+            </p>
+          ))}
+        </div>
+      )}
+      {isSkillEmpty ? (
+        ""
+      ) : (
+        <div id="skills">
+          <h2 className="section-title">Skills</h2>
+          {skills.map((skill) => {
+            if (!skill.name) return "";
+            return (
+              <div key={skill.id} className="skill-field">
+                <p>{skill.name}</p>
+                <div className="rating-meter">
+                  <div
+                    className="rating-meter-fill"
+                    style={{ width: `${skill.rating * 10}%` }}
+                  ></div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
