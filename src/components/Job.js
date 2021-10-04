cbb01e5764d3d3a678f6acf08c54bc4805ad6afa
cyanchill <anthonyliang9@gmail.com>
2021-10-03 21:51:38 -0400

    completed main content section of cv

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { CVContext } from "../CVContext";
 
-import { FaTrash } from "react-icons/fa";
+import { getTermDate } from "../helper/utilitiy";
 
 const Job = ({ deleteJob, experience }) => {
   const { preview, cvInfo, setCVInfo } = useContext(CVContext);
@@ -22,38 +22,59 @@ const Job = ({ deleteJob, experience }) => {
     });
   };
 
+  if (!preview) {
+    return (
+      <div className="job">
+        <div className="text flex-col">
+          <input
+            type="text"
+            placeholder="Job Title"
+            value={title}
+            onChange={(e) => updateField(e, "title")}
+          />
+          <input
+            type="text"
+            placeholder="Company Name"
+            value={company}
+            onChange={(e) => updateField(e, "company")}
+          />
+          <textarea
+            className="description-field"
+            placeholder="Job Description"
+            rows="2"
+            value={description}
+            onChange={(e) => updateField(e, "description")}
+          ></textarea>
+        </div>
+        <div className="dates flex-col">
+          <span className="field-title">Start Date:</span>
+          <input type="date" value={startDate} onChange={(e) => updateField(e, "startDate")} />
+          <span className="field-title">End Date:</span>
+          <input type="date" value={endDate} onChange={(e) => updateField(e, "endDate")} />
+          <button className="btn trash" onClick={() => deleteJob(id)}>
+            Delete
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="job">
-      <div className="text flex-col">
-        <input
-          type="text"
-          placeholder="Job Title"
-          value={title}
-          onChange={(e) => updateField(e, "title")}
-        />
-        <input
-          type="text"
-          placeholder="Company Name"
-          value={company}
-          onChange={(e) => updateField(e, "company")}
-        />
-        <textarea
-          className="description-field"
-          placeholder="Job Description"
-          rows="2"
-          value={description}
-          onChange={(e) => updateField(e, "description")}
-        ></textarea>
-      </div>
-      <div className="dates flex-col">
-        <span className="field-title">Start Date:</span>
-        <input type="date" value={startDate} onChange={(e) => updateField(e, "startDate")} />
-        <span className="field-title">End Date:</span>
-        <input type="date" value={endDate} onChange={(e) => updateField(e, "endDate")} />
-        <button className="btn trash" onClick={() => deleteJob(id)}>
-          Delete
-        </button>
-      </div>
+      <h1 className="job-name">
+        {title} @{company}
+      </h1>
+      {!startDate ? (
+        ""
+      ) : !endDate ? (
+        <p className="duration">{getTermDate(startDate)} -</p>
+      ) : (
+        <p className="duration">
+          {getTermDate(startDate)} - {getTermDate(endDate)}
+        </p>
+      )}
+
+      <p className="description">{description}</p>
     </div>
   );
 };
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,11 +1,46 @@
-import Education from "./Education";
-import Experience from "./Experience";
+import { useContext } from "react";
+import { CVContext } from "../CVContext";
+
+import School from "./School";
+import Job from "./Job";
+import "../styles/MainContent.style.css";
 
 const MainContent = () => {
+  const {
+    cvInfo: { education, experience },
+  } = useContext(CVContext);
+
+  const isEducationEmpty = education.every((obj) => {
+    obj.id = "";
+    return Object.values(obj).every((val) => val === "");
+  });
+  const isExperienceEmpty = experience.every((obj) => {
+    obj.id = "";
+    return Object.values(obj).every((val) => val === "");
+  });
+
   return (
-    <div className="main-content">
-      <Experience />
-      <Education />
+    <div id="main-content">
+      {isExperienceEmpty ? (
+        ""
+      ) : (
+        <div id="experience">
+          <h2 className="section-title">Experience</h2>
+          {experience.map((job) => (
+            <Job experience={job} key={job.id} />
+          ))}
+        </div>
+      )}
+      {isEducationEmpty ? (
+        ""
+      ) : (
+        <div id="education">
+          <h2 className="section-title">Education</h2>
+          {education.map((school) => (
+            <School education={school} key={school.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
